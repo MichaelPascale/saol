3791b7ffa9541090641aff9633daf1dba3bcbaa8
MichaelPascale <mpascale@bu.edu>
2025-04-02 20:37:11 -0400

    feats: sketch hex wall mesh generation

--- Assets/HexTiles.cs
+++ Assets/HexTiles.cs
@@ -3,51 +3,111 @@ using System.Collections.Generic;
 using System.IO.Compression;
 using Unity.Mathematics;
 using Unity.VisualScripting;
+using UnityEditor.Build;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.InputSystem.Interactions;
 using UnityEngine.UIElements;
 using static System.Math;
 
 public class HexTile3D
 {
+    public string name;
     public Vector3 location;
     public readonly GameObject gobj;
 
-    public HexTile3D(string name, Vector3 where, uint walls=0x7F) {
+    private const float HEIGHT = 1;
+    private enum Directions { SouthEast, South, SouthWest, NorthWest, North, NorthEast };
+
+    public HexTile3D(string name, Vector3 where, uint wflags = 0xFF) {
         gobj = new GameObject(name);
+        this.name = name;
+        location = where;
 
         gobj.AddComponent<MeshFilter>();
         gobj.AddComponent<MeshRenderer>();
         gobj.AddComponent<MeshCollider>();
 
-        // Create a mesh for the hexagonal floor.
-        Mesh mesh = gobj.GetComponent<MeshFilter>().mesh;
-        mesh.Clear();
+        {
+            // Create a mesh for the hexagonal floor.
+            Mesh mesh = gobj.GetComponent<MeshFilter>().mesh;
+            mesh.Clear();
 
-        // Allocate a center and six points.
-        Vector3[]
-        vertices = new Vector3[7];
+            // Allocate a center and six points.
+            List<Vector3>
+            vertices = new List<Vector3>();
 
-        Vector2[]
-        uvs = new Vector2[7];
+            // Define the vertices of the hexagon in clockwise fashion.
+            // Unity assumes the front face of a mesh has clockwise triangle winding.
+            vertices.Add(Vector3.zero);
+            for (uint i = 0; i < 6; i++) vertices.Add(new Vector3((float)Cos(-i*PI/3), 0, (float)Sin(-i*PI/3)));
 
-        // Define the vertices of the hexagon in clockwise fashion.
-        // Unity assumes the front face of a mesh has clockwise triangle winding.
-        vertices[0] = Vector3.zero;
-        for (uint i = 0; i < 6; i++) vertices[i+1] = new Vector3((float)Cos(-i*PI/3), 0, (float)Sin(-i*PI/3));
+            mesh.vertices = vertices.ToArray();
 
-        mesh.vertices = vertices;
+            // A hexagon is six triangles, six triplets of vertices.
+            List<int>
+            triangles = new List<int>();
+            for (int i = 0; i < 6; i++) triangles.AddRange(new int[] {0, i + 1, i < 5 ? i + 2 : 1});
 
-        // A hexagon is six triangles, six triplets of vertices.
-        int[]
-        triangles = new int[18];
-        for (int i = 0; i < 6; i++) {
-            triangles[i*3] = 0;
-            triangles[i*3+1] = i + 1;
-            triangles[i*3+2] = i < 5 ? i + 2 : 1;
+            mesh.triangles = triangles.ToArray();
+        }
+    
+        // If a ceiling is specified (seventh bit), then duplicate the floor.
+        // if ((walls & 0x40) > 0) {
+        //     for (uint i = 0; i < 6; i++) vertices.Add(new Vector3((float)Cos(i*PI/3), 1, (float)Sin(i*PI/3)));
+        //     for (int i = 7; i < 13; i++) triangles.AddRange(new int[] {7, i + 1, i < 5 ? i + 2 : 8});
+        // }
+
+        // Add a wall on the far face of each triangle, according to the walls flag.
+        // Six binary flags (two octal digits) set the clockwise wall presence:
+        //     southeast, south, southwest, northwest, north, northeast
+        {
+            GameObject walls = new GameObject("walls");
+            walls.AddComponent<MeshFilter>();
+            walls.AddComponent<MeshRenderer>();
+            walls.AddComponent<MeshCollider>();
+            Mesh mesh = walls.GetComponent<MeshFilter>().mesh;
+            mesh.Clear();
+
+            List<Vector3>
+            vertices = new List<Vector3>();
+
+            List<int>
+            triangles = new List<int>();
+
+            int wcount = 0;
+            for (int i = 0; i < 6; i++) {
+                if ((wflags & 0x1u) > 0) {
+                    
+                    // Create Quads, starting from the bottom left corner of each.
+                    vertices.AddRange(new Vector3[] {
+                        new Vector3((float)Cos(-i*PI/3),     0,         (float)Sin(-i*PI/3)),
+                        new Vector3((float)Cos(-i*PI/3),     HEIGHT,    (float)Sin(-i*PI/3)),
+                        new Vector3((float)Cos(-(i+1)*PI/3), HEIGHT,    (float)Sin(-(i+1)*PI/3)),
+                        new Vector3((float)Cos(-(i+1)*PI/3), 0,         (float)Sin(-(i+1)*PI/3)),
+                    });
+
+                    // Add triangles.
+                    // Vertex indicies depend on how many walls have been created so far.
+                    int o = (i-wcount)*4;
+                    triangles.AddRange(new int[] {
+                        o+0, o+1, o+2,
+                        o+2, o+3, o+0
+                    });
+
+                    wcount++;
+                }
+                
+
+                wflags = wflags >> 1;
+            }
+
+            mesh.vertices = vertices.ToArray();
+            mesh.triangles = triangles.ToArray();
+            walls.transform.parent = gobj.transform;
         }
 
-        mesh.triangles = triangles;
+        gobj.transform.position = where;
     }
 }
 
@@ -56,8 +116,15 @@ public class HexTiles : MonoBehaviour
 {
     void Start()
     {
-        HexTile3D h = new HexTile3D("test hex", Vector3.zero, 0x15);
-        print(h.gobj.GetComponent<MeshFilter>().mesh.uv);
+        HexTile3D h = new HexTile3D("test hex", Vector3.zero, 0x37);
+        new HexTile3D("northeast", new Vector3(2, 0, 1), 0x3A);
+        new HexTile3D("east", new Vector3(0, 0, 3), 0x1F);
+
+
+        new HexTile3D("other", new Vector3(6, 1, 6), 0xFF);
+
+        new HexTile3D("other", new Vector3(-6, 1, -6), 0x00);
+
     }
 
     void Update()
