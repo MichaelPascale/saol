7e44558cf3b34f691b35fc6a8982422695489fde
MichaelPascale <mpascale@bu.edu>
2026-01-27 11:43:34 -0500

    feat: compress tsv saves

--- Assets/Scripts/Util/TSVWriter.cs
+++ Assets/Scripts/Util/TSVWriter.cs
@@ -6,16 +6,18 @@
  */
 
 using System;
+using System.IO.Compression;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using UnityEngine;
-public class TableWriter<T>
+using K4os.Compression.LZ4.Internal;
+public class TableWriter<ITuple>
 {
 
     public readonly string[] headers;
     public readonly int ncol;
-    public readonly Func<T, string> formatter;
+    public readonly Func<ITuple, string> formatter;
 
     public readonly string delim;
     public readonly string newline;
@@ -23,7 +25,7 @@ public class TableWriter<T>
     private List<string> data;
 
     // If the user specifies headers, the headers array determines the number of columns.
-    public TableWriter(string[] headers, Func<T, string> formatter, string delim = "\t", string newline = "\n")
+    public TableWriter(string[] headers, Func<ITuple, string> formatter, string delim = "\t", string newline = "\n")
     {
         this.headers = headers;
         ncol = headers.Length;
@@ -34,7 +36,7 @@ public class TableWriter<T>
         data = new() { string.Join(delim, headers) };
     }
     // Otherwise, there is no header row and the user must specify the number of columns.
-    public TableWriter(int ncol, Func<T, string> formatter, string delim = "\t", string newline = "\n")
+    public TableWriter(int ncol, Func<ITuple, string> formatter, string delim = "\t", string newline = "\n")
     {
         this.formatter = formatter;
         this.delim = delim;
@@ -44,23 +46,42 @@ public class TableWriter<T>
         data = new();
     }
 
-    public TableWriter<T> append(T item)
+    public TableWriter<ITuple> append(ITuple item)
     {
         data.Add(formatter(item));
         return this;
     }
 
-    public TableWriter<T> append(List<T> items)
+    public TableWriter<ITuple> append(List<ITuple> items)
     {
-        foreach (T item in items) append(item);
+        foreach (ITuple item in items) append(item);
         return this;
     }
 
-    public TableWriter<T> write(string filename)
+    public string serialize()
     {
+        return string.Join(newline, data);
+    }
+
+    public TableWriter<ITuple> write(string filename)
+    {
+
+        if (filename.EndsWith(".gz"))
+        {
+            using (FileStream stream = File.Create(Path.Combine(Application.persistentDataPath, filename)))
+            using (GZipStream compressor = new GZipStream(stream, System.IO.Compression.CompressionLevel.Optimal))
+            using (StreamWriter writer = new StreamWriter(compressor, Encoding.UTF8))
+            {
+                foreach (var line in data)
+                    writer.WriteLine(line);
+            }
+
+            return this;
+        }
+
         File.AppendAllText(
             Path.Combine(Application.persistentDataPath, filename),
-            string.Join(newline, data),
+            serialize(),
             Encoding.UTF8
         );
 
