0d6fce71667295b7830a7428497970d366406420
MichaelPascale <mpascale@bu.edu>
2026-01-29 11:46:53 -0500

    feat: save and load stim order file commands

--- Assets/Scripts/Experiment/Experiment.cs
+++ Assets/Scripts/Experiment/Experiment.cs
@@ -14,17 +14,51 @@ public abstract class SAOLExperiment : MonoBehaviour
     public enum Mode { Uninitialized, Stopped, Running }
     protected Mode mode;
     protected uint trial;
+    protected uint n_trials;
     protected DateTime t_init;
-    protected void start()
+    protected DateTime t_trial;
+    protected double elapsed_s;
+    protected double elapsed_trial_s;
+
+    public void Start()
+    {
+        
+    }
+    public void Update()
+    {
+        if (mode != Mode.Running)
+            return;
+
+        elapsed_s = DateTime.Now.Subtract(t_init).TotalSeconds;
+        elapsed_trial_s = DateTime.Now.Subtract(t_trial).TotalSeconds;
+    }
+
+    protected void onstart()
     {
         trial = 0;
         mode = Mode.Running;
         t_init = DateTime.Now;
+        elapsed_s = 0;
+        
+        t_trial = DateTime.Now;
+        elapsed_trial_s = 0;
     }
 
-    protected void stop()
+    protected void onstop()
     {
         mode = Mode.Stopped;
+    }
+
+    protected void next_trial()
+    {
+        trial++;
+
+        if (trial == n_trials)
+            // FIXME: Implement onend() method to handle this.
+            Console.WriteLine("ENDED");
+
+        t_trial = DateTime.Now;
+        elapsed_trial_s = 0;
     }
 
     public abstract string cmd_save(string path);
