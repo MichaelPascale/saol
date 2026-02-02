eb9173eafec478906c51b26e74ccbd19dfa4fc18
MichaelPascale <mpascale@bu.edu>
2026-02-02 13:48:45 -0500

    feat(overlay): show and hide text instructions

--- Assets/Scripts/Experiment/Overlay.cs
+++ Assets/Scripts/Experiment/Overlay.cs
@@ -6,57 +6,72 @@
  */
 
 using System.Collections.Generic;
-using Unity.Collections;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Rendering;
 using UnityEngine.UI;
 
 public class SAOLOverlay : MonoBehaviour
 {
-    public bool isVisible = false;
     public Camera cam;
     protected Canvas canvas;
-    protected Image bg_image;
-    protected List<Text> fg_elements;
+    protected Image bg;
+    protected List<GameObject> fg_elements = new List<GameObject>();
     
     void Start()
     {
         canvas = gameObject.AddComponent<Canvas>();
         canvas.renderMode = RenderMode.ScreenSpaceOverlay;
 
-        bg_image = canvas.AddComponent<Image>();
-        bg_image.color = new Color(0,0,0,211);
+        var obj = new GameObject("Background Image");
+        obj.transform.SetParent(transform);
+        bg = obj.AddComponent<Image>();
+        bg.color = new Color(0,0,0,.75f);
 
-        text("5", 0.2f, 0.2f);
+        RectTransform rect = obj.GetComponent<RectTransform>();
+        rect.anchorMin = Vector2.zero;
+        rect.anchorMax = Vector2.one;
+        rect.offsetMin = Vector2.zero;
+        rect.offsetMax = Vector2.zero;
+        rect.sizeDelta = Vector2.zero;
+
+        hide();
     }
 
-    void show()
+    public void show()
     {
         gameObject.SetActive(true);
-        isVisible = true;
     }
 
-    void hide()
+    public void hide()
     {
         gameObject.SetActive(false);
     }
-    void text(string message, float x = .5f, float y = .5f, Color color = default)
+    public void text(string message, Color color = default)
     {
         if (color == default)
             color = Color.white; // Using default because function signature must be compile-time constant.
 
-        Text text = gameObject.AddComponent<Text>();
+        var obj = new GameObject("Text Display");
+        obj.transform.SetParent(transform);
+        Text text = obj.AddComponent<Text>();
         text.text = message;
         text.color = color;
-        text.rectTransform.pivot = new Vector2(x,y);
-        fg_elements.Add(text);
+        text.font = Resources.GetBuiltinResource<Font>("LegacyRuntime.ttf");
+        text.alignment = TextAnchor.MiddleCenter;
+        text.fontSize = 32;
+
+        RectTransform rect = obj.GetComponent<RectTransform>();
+        rect.anchorMin = new Vector2(0.25f, 0.25f);
+        rect.anchorMax = new Vector2(0.75f, 0.75f);
+        rect.offsetMin = Vector2.zero;
+        rect.offsetMax = Vector2.zero;
+
+        fg_elements.Add(obj);
     }
 
-    void clear()
+    public void clear()
     {
-        foreach (Text text in fg_elements)
-            Destroy(text);
+        foreach (GameObject obj in fg_elements)
+            Destroy(obj);
         fg_elements.Clear();
     }
 
