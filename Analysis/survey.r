d619a32d2933c47f7243b3628b2f2348e6fb24b0
MichaelPascale <mpascale@bu.edu>
2026-04-03 17:30:03 -0400

    refactor(analysis): edit draft of questionnaire processing

--- Analysis/survey.r
+++ Analysis/survey.r
@@ -1,33 +1,87 @@
-# CSV Data.R (03.20).R Alev Egilmez 
-library(tidyverse)
-qualtrics <- read_csv("/Users/caglalev/Desktop/CD Lab/PCRM Survey Data/PCRMSP26+Pilot_March+19,+2026_18.50.csv")
-qualtrics <- qualtrics [c(-1,-2),]
-dcr_scale <- qualtrics |>
-  select(id = subject_id...11, "5DCR_1":"5DCR_24") |>
+# survey.r
+# Process and summarize surveys 
+#
+# Copyright (c) 2026, Michael P. Pascale <mpascale@bu.edu>.
+# Copyright (c) 2026, Alev Egilmez <caglalev@bu.edu>.
+# SPDX-License-Identifier: MIT
+
+survey <- survey |>
+  mutate(subject = str_remove_all(subject_id...11, "_|\\s"), .before=1) |>
+  rename_with(str_remove_all, pattern="\\s") |>
+  # Name the questions that were derived from Jepma et al. (2012).
+  rename_with(\(x) paste0("Jepma_",str_extract(x, "\\d+$")), starts_with("ArtMuseumGame")) |>
+  # Remove the leading "5" from the 5DCR variable names, just to avoid having to quote all the time.
+  rename_with(str_remove, starts_with("5DCR"), "^5") |>
+  select(
+    subject,
+    gameExpYears=GamingExperience,
+    gameSkill=GamingSkill,
+    starts_with("Jepma"),
+    starts_with("DCR"),
+    starts_with("AEQ"),
+    starts_with("AReA")
+  )
+
+# Questions from Jepma et al. (2012).
+labels <- c(
+  Jepma_1 = "curious about blurred",
+  Jepma_2 = "tried to guess",
+  Jepma_3 = "disappointed on no clear*", # NOTE: This one doesn't really make sense in this task"
+  Jepma_4 = "recognized blurred objects",
+  Jepma_5 = "tried to remember"
+)
+
+survey |>
+  pivot_longer(starts_with("Jepma"), names_transform=\(x) factor(x, names(labels))) |>
+  ggplot() +
+  geom_histogram(aes(value)) +
+  facet_wrap(vars(name), labeller=as_labeller(labels))
+
+  survey |> #names_transform=\(x) factor(name, names(labels), labels)
+    pivot_longer(starts_with("Jepma")) |>
+    mutate(name = factor(name, names(labels), labels)) |>
+    ggplot() +
+    stat_summary(aes(value, name), orientation="y", fun.data=partial(mean_sdl, mult=1)) +
+    scale_x_continuous("Scale from 1 (Not at All) to 5 (Very Much)", limits=c(0,6), breaks=1:5) +
+    scale_y_discrete(limits=rev) +
+    labs(
+      title = "Average response on five questions from Jepma et al. (2012), Table 1",
+      caption = "* The question was asked but does not apply in this experiment"
+    )
+
+
+# Questions from Kashdan
+survey |>
+  select(subject, DCR_1:DCR_24) |>
   mutate(
-       .before = "5DCR_1",
-       joy = rowMeans(across("5DCR_1":"5DCR_4", as.numeric)), 
-       ds = rowMeans(across("5DCR_5":"5DCR_8", as.numeric)), 
-       st = rowMeans(across("5DCR_9":"5DCR_12", ~ 8 - as.numeric(.x))),  
-       ts = rowMeans(across("5DCR_13":"5DCR_16", as.numeric)),
-       osc = rowMeans(across("5DCR_17":"5DCR_20", as.numeric)),
-       csc = rowMeans(across("5DCR_21":"5DCR_24", as.numeric)))
-aeq_scale <- qualtrics |>
-  select(subject_id...11, "AEQ_1":"AEQ_22") |>
-mutate(
-      .before = "AEQ_1",
-      total = rowMeans(across("AEQ_1":"AEQ_22", as.numeric)),
-      emotional = rowMeans(across("AEQ_1":"AEQ_4", as.numeric)),
-      cultural = rowMeans(across("AEQ_5":"AEQ_8", as.numeric)),
-      perceptual = rowMeans(across("AEQ_9":"AEQ_11", as.numeric)),
-      understanding = rowMeans(across("AEQ_12":"AEQ_15", as.numeric)),
-      proximal = rowMeans(across("AEQ_16":"AEQ_18", as.numeric)),
-      experience = rowMeans(across("AEQ_19":"AEQ_22", as.numeric)))
-AReA_scale <- qualtrics |>
-  select(subject_id...11, "AReA_1":"AReA_14") |>
-mutate(
-      .before = "AReA_1",
-      aa = rowMeans(across(c("AReA_1","AReA_2","AReA_3","AReA_4","AReA_6","AReA_9","AReA_13","AReA_14"), as.numeric)),
-      iae = rowMeans(across(c("AReA_8","AReA_11","AReA_12",AReA_13),as.numeric)),
-      cb = rowMeans(across(c("AReA_5","AReA_7","AReA_10"), as.numeric))
-        )
+    .after = 1,
+    JE  = rowMeans(across(DCR_1:DCR_4)),
+    DS  = rowMeans(across(DCR_5:DCR_8)),
+    ST  = rowMeans(across(DCR_9:DCR_12, ~ 8 - .x)),
+    TS  = rowMeans(across(DCR_13:DCR_16)),
+    OSC = rowMeans(across(DCR_17:DCR_20)),
+    CSC = rowMeans(across(DCR_21:DCR_24))
+  )
+
+# Questions from aesthetics questionnaires
+survey |>
+  select(subject, AEQ_1:AEQ_22) |>
+  mutate(
+    .after = 1,
+    total         = rowMeans(across(AEQ_1:AEQ_22)),
+    emotional     = rowMeans(across(AEQ_1:AEQ_4)),
+    cultural      = rowMeans(across(AEQ_5:AEQ_8)),
+    perceptual    = rowMeans(across(AEQ_9:AEQ_11)),
+    understanding = rowMeans(across(AEQ_12:AEQ_15)),
+    proximal      = rowMeans(across(AEQ_16:AEQ_18)),
+    experience    = rowMeans(across(AEQ_19:AEQ_22))
+  )
+
+survey |>
+  select(subject, AReA_1:AReA_14) |>
+  mutate(
+    .after = 1,
+    aa  = rowMeans(across(c(AReA_1, AReA_2, AReA_3,  AReA_4, AReA_6, AReA_9, AReA_13, AReA_14))),
+    iae = rowMeans(across(c(AReA_8, AReA_11,AReA_12, AReA_13))),
+    cb  = rowMeans(across(c(AReA_5, AReA_7, AReA_10)))
+  )
